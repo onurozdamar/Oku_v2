301a97387f1bd2dfd2f1963fafbfe857c0ca2d90
Onur Özdamar <63419558+onurozdamar@users.noreply.github.com>
2022-06-15 14:23:48 +0300

    order by

--- src/database/index.js
+++ src/database/index.js
@@ -117,7 +117,8 @@ export class BaseManager {
     return new Promise((resolve, reject) => {
       this.openDatabase().then(db => {
         db.executeSql(
-          'SELECT *, Book.bookId FROM Book LEFT OUTER JOIN History ON History.bookId = Book.bookId WHERE Book.currentPage>0 AND Book.currentPage<page GROUP BY Book.bookId ORDER BY readDate DESC',
+          'SELECT *, Book.bookId FROM Book LEFT OUTER JOIN History ON History.bookId = Book.bookId' +
+            ' WHERE Book.currentPage>0 AND Book.currentPage<page GROUP BY Book.bookId ORDER BY readDate DESC',
         )
           .then(([values]) => {
             var array = [];
@@ -390,7 +391,9 @@ export class BaseManager {
   getHistories(bookId) {
     return new Promise((resolve, reject) => {
       this.openDatabase().then(db => {
-        db.executeSql(`SELECT * FROM History WHERE bookId=${bookId}`)
+        db.executeSql(
+          `SELECT * FROM History WHERE bookId=${bookId} ORDER BY readDate DESC`,
+        )
           .then(([values]) => {
             var array = [];
 
